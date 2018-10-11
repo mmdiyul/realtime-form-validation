769152563b403de9050893d6cda0f598d28720d4
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-10-11 21:17:41 +0700

    add password confirmation :key:

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ function nameValidation(){
         document.getElementById('helpNama').setAttribute('class', 'fa fa-times text-danger')
         document.getElementById('helpNama').innerHTML = ' Masukkan nama lengkap'
     } else {
-        document.getElementById('helpNama').innerHTML = ' Nama telah valid'
         document.getElementById('helpNama').setAttribute('class', 'fa fa-check text-success')
+        document.getElementById('helpNama').innerHTML = ' Nama telah valid'
     }
 }
 
@@ -15,8 +15,8 @@ function usernameValidation() {
         document.getElementById('helpUsername').setAttribute('class', 'fa fa-times text-danger')
         document.getElementById('helpUsername').innerHTML = ' Masukkan nama pengguna minimal 5 karakter'
     } else {
-        document.getElementById('helpUsername').innerHTML = ' Nama pengguna telah valid'
         document.getElementById('helpUsername').setAttribute('class', 'fa fa-check text-success')
+        document.getElementById('helpUsername').innerHTML = ' Nama pengguna telah valid'
     }
 }
 
@@ -24,11 +24,11 @@ function emailValidation() {
     let email = document.getElementById('email').value
     // regex for if-else condition from https://stackoverflow.com/a/4964763
     if(/(.+)@(.+){2,}\.(.+){2,}/.test(email)) {
-        document.getElementById('helpEmail').innerHTML = ' Email telah valid'
         document.getElementById('helpEmail').setAttribute('class', 'fa fa-check text-success')      
+        document.getElementById('helpEmail').innerHTML = ' Email telah valid'
     } else {
-        document.getElementById('helpEmail').innerHTML = ' Masukkan email yang valid'
         document.getElementById('helpEmail').setAttribute('class', 'fa fa-times text-danger')
+        document.getElementById('helpEmail').innerHTML = ' Masukkan email yang valid'
     }
 }
 
@@ -38,7 +38,22 @@ function passwordValidation() {
         document.getElementById('helpPassword').setAttribute('class', 'fa fa-times text-danger')
         document.getElementById('helpPassword').innerHTML = ' Kata sandi minimal 8 karakter'
     } else {
-        document.getElementById('helpPassword').innerHTML = ' Kata sandi telah memenuhi minimal 8 karakter'
         document.getElementById('helpPassword').setAttribute('class', 'fa fa-check text-success')
+        document.getElementById('helpPassword').innerHTML = ' Kata sandi telah memenuhi minimal 8 karakter'
+    }
+}
+
+function passwordConfirm() {
+    let password = document.getElementById('password').value
+    let passwordConfirm = document.getElementById('passwordConfirmation').value
+    if (passwordConfirm == "" || passwordConfirm == password && passwordConfirm.length < 8) {       
+        document.getElementById('helpPasswordConfirmation').setAttribute('class', 'fa fa-times text-danger')
+        document.getElementById('helpPasswordConfirmation').innerHTML = ' Kata sandi harus memenuhi kriteria'
+    } else if (passwordConfirm == password) {
+        document.getElementById('helpPasswordConfirmation').setAttribute('class', 'fa fa-check text-success') 
+        document.getElementById('helpPasswordConfirmation').innerHTML = ' Kata sandi cocok'
+    } else {
+        document.getElementById('helpPasswordConfirmation').setAttribute('class', 'fa fa-times text-danger')
+        document.getElementById('helpPasswordConfirmation').innerHTML = ' Kata sandi tidak cocok'
     }
 }
